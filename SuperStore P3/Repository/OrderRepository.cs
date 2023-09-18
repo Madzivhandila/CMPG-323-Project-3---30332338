86c0395822381db32550cc30a985b665ff94d4c0
Tendani Archebold Madzivhandila <75025282+Madzivhandila@users.noreply.github.com>
2023-09-18 10:14:33 +0200

    Added Class

---
+++ SuperStore P3/Repository/OrderRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EcoPower_Logistics.Repository.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string DeliveryAddress { get; set; } = null!;
+        public DateTime OrderDate { get; set; }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = null!;
+    }
+
+    public class OrderRepository
+    {
+        private List<Order> orders;
+
+        public OrderRepository()
+        {
+            orders = new List<Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            orders.Add(order);
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            var order = orders.FirstOrDefault(o => o.OrderId == orderId);
+            if (order == null)
+            {
+                throw new InvalidOperationException("Order not found");
+            }
+            return order;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return orders;
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            var existingOrder = orders.FirstOrDefault(o => o.OrderId == order.OrderId);
+            if (existingOrder != null)
+            {
+                // Update individual properties of the existing order
+                existingOrder.DeliveryAddress = order.DeliveryAddress;
+                existingOrder.OrderDate = order.OrderDate;
+            }
+        }
+    }
+}
---
+++ SuperStore P3/Repository/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductDescription { get; set; } = null!;
+        public string ProductName { get; set; } = null!;
+    }
+
+    public class ProductRepository
+    {
+        private List<Product> products;
+
+        public ProductRepository()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public Product GetProductById(int productId)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == productId);
+            if (product == null)
+            {
+                throw new InvalidOperationException("Product not found");
+            }
+            return product;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.ProductId == product.ProductId);
+            if (existingProduct != null)
+            {
+                existingProduct.ProductDescription = product.ProductDescription;
+                existingProduct.ProductName = product.ProductName;
+            }
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                products.Remove(product);
+            }
+        }
+    }
+}
