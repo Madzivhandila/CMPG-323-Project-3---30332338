9a4d10909fc6958084950533a64a2d51c573f4f8
Tendani Archebold Madzivhandila <75025282+Madzivhandila@users.noreply.github.com>
2023-09-18 11:34:57 +0200

    add zone controller

---
+++ SuperStore P3/Controllers/ZonesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using EcoPower_Logistics.Repository; // Import the Products Repository namespace
+
+namespace EcoPower_Logistics.Controllers
+{
+    [Route("api/zones")]
+    [ApiController]
+    public class ZonesController : ControllerBase
+    {
+        private readonly CustomerRepository customerRepository; // Inject the Customers Repository
+
+        public ZonesController(CustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        [HttpGet("customers/{customerId}")]
+        public ActionResult GetCustomer(int customerId)
+        {
+            try
+            {
+                var customer = customerRepository.GetCustomerById(customerId);
+                return Ok(customer);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("customers")]
+        public ActionResult<IEnumerable<Customer>> GetCustomers()
+        {
+            var customers = customerRepository.GetAllCustomers();
+            return Ok(customers);
+        }
+    }
+}
