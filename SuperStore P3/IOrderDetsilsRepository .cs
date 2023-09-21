1662f09e064a1bc64ae1ac2d71ca729df3caad06
Tendani Archebold Madzivhandila <75025282+Madzivhandila@users.noreply.github.com>
2023-09-21 11:18:21 +0200

    Fixing codes

--- SuperStore P3/IOrderDetsilsRepository .cs
+++
@@ -1,9 +0,0 @@
-﻿using EcoPower_Logistics.Repository.Models;
-
-namespace EcoPower_Logistics
-{
-    public interface IOrderDetsilsRepository : IGenericRepository<OrderDetails>
-    {
-        OrderDetails GetMostRecentService();
-    }
-}
--- SuperStore P3/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using EcoPower_Logistics.Repository.Models;
-
-namespace EcoPower_Logistics
-{
-    public interface IOrdreRepositoryI : IGenericRepository<OrderDetails>
-    {
-        OrderDetails GetMostRecentService();
-    }
-}
